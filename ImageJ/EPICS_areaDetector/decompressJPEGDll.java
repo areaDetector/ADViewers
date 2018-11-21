d349de0befba75d89448a672e0d5094a0164caa9
Mark Rivers <rivers@cars.uchicago.edu>
2018-11-21 10:14:52 -0600

    New file

---
+++ ImageJ/EPICS_areaDetector/decompressJPEGDll.java
@@ -0,0 +1,21 @@
+import java.nio.Buffer;
+import com.sun.jna.Native;
+import com.sun.jna.NativeLong;
+
+public class decompressJPEGDll {
+
+	static {
+		Native.register("jpegDecompress" + getArchPlatform());
+	}
+
+	public static String getArchPlatform() {
+		String archDataModel = System.getProperty("sun.arch.data.model");
+		if (archDataModel.equals("64")) {
+			archDataModel = "";
+		}
+		return archDataModel;
+	}
+
+	public static native int jpegDecompress(Buffer src, NativeLong srcSize, Buffer dest, NativeLong destSize);
+
+}
